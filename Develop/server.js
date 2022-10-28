3b07b660f3e9093fba9e2f74394363fbe852b992
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-28 09:15:20 -0700

    enter the right variable into the get and the post db/db.json file

--- Develop/server.js
+++ Develop/server.js
@@ -19,10 +19,9 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-
 app.get('/api/notes', (req, res) => {
-    console.log(reviews);
-    res.json(reviews);
+    console.log(dbNotes);
+    res.json(dbNotes);
     console.info(`${req.method} request received to get reviews`);
 });
 
