94679ac3f2f4f773a3ad3d1fc3e86a781328ebf5
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-28 21:09:51 -0700

    change port to 3001

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require('path');
 var uniqid = require("uniqid");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
